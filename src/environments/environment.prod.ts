44510f9c39fa037bceb3a1bbcb32b2182e4200f8
espe <esperanza.del.junco@adaits.es>
2021-03-07 12:20:41 +0100

    added enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDEyQApPTbvDo2fLtejq3BjwMQHmdGL6yM",
+    authDomain: "angular-6cc27.firebaseapp.com",
+    databaseURL: "https://angular-6cc27-default-rtdb.firebaseio.com",
+    projectId: "angular-6cc27",
+    storageBucket: "angular-6cc27.appspot.com",
+    messagingSenderId: "412253714169",
+    appId: "1:412253714169:web:98952bf887829da51c5428",
+    measurementId: "G-GFCQL595L2"
+  },
+};
